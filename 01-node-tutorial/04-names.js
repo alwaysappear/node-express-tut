e028b7a055eb89ba7581d878dd51925c094e962c
Raheem Kawojue <kawojue08@gmail.com>
2023-01-02 17:33:52 +0100

    [add] names modul

---
+++ 01-node-tutorial/04-names.js
@@ -0,0 +1,11 @@
+//local
+const secret = 'SUPER USER'
+
+// share
+const aa = "Always Appear"
+const rok = "Kawojue Raheem Olumuyiwa"
+const babe = "Mary Anuoluwapo"
+
+module.exports = {aa, rok, babe}
+
+console.log(module)

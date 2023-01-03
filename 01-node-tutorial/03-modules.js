dea5079c2cef990958e2ebbc55e74d6a8f03f611
Raheem Kawojue <kawojue08@gmail.com>
2023-01-03 23:38:30 +0100

    auto invoke

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,8 +1,14 @@
 const names = require('./04-names')
 const greet = require('./05-utils')
+const data = require('./06-alt')
+
+require('./07-mind-grenade')
 
 const {aa, babe, rok} = names
 
 greet(aa)
 greet(rok)
 greet(babe)
+
+
+console.log(data)

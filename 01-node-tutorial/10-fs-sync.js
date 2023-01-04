41fb14ffd35b3b08d84c34888c9d4771f0cfb35e
Raheem Kawojue <kawojue08@gmail.com>
2023-01-04 12:46:22 +0100

    {flag: 'a'} append text

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -8,3 +8,9 @@ writeFileSync(
     './content/result-sync.txt',
     `Here is the result: \n\t1. ${first}\n\t2. ${second}`
 )
+
+writeFileSync(
+    './content/result-sync.txt',
+    '\nThis is an appended text.',
+    {flag: 'a'}
+)

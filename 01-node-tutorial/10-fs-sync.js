208c291e12fa1cc868a11be3391bd8b527f7128a
Raheem Kawojue <kawojue08@gmail.com>
2023-01-04 11:57:49 +0100

    reading txt files

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -1,3 +1,8 @@
 const fs = require('fs')
 const {readFileSync, writeFileSync} = fs
 
+const first = readFileSync('./content/first.txt', 'utf-8')
+const second = readFileSync('./content/second.txt', 'utf-8')
+
+console.log(first)
+console.log(second)

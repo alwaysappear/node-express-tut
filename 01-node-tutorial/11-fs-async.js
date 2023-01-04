b9629d81f647aef68345118c53d827674da41e33
Raheem Kawojue <kawojue08@gmail.com>
2023-01-04 23:55:34 +0100

    [write] file

--- 01-node-tutorial/11-fs-async.js
+++ 01-node-tutorial/11-fs-async.js
@@ -1,6 +1,29 @@
 const {readFile, writeFile} = require('fs')
+const path = require('path')
 
-readFile('./content/first.txt', 'utf-8', (err, result) => {
+const resultPath = path.resolve(__dirname, 'content', 'result-async.txt')
+
+readFile(
+    './content/first.txt',
+    'utf-8',
+    (err, result) => {
     if (err) throw (err)
-    console.log(result)
+
+    const first = result
+    readFile(
+        './content/second.txt', 
+        'utf-8',
+        (err, result) => {
+            if (err) throw (err)
+
+            const second = result
+            writeFile(
+                resultPath,
+                `Here are the results: \n\t1. ${first}\n\t2. ${second}`,
+                (err) => {
+                    if (err) throw (err)
+                }
+            )
+        }
+    )
 })

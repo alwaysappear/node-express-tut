1c52f91f1416ce1f8145bed513dab55d8b69a13c
Raheem Kawojue <kawojue08@gmail.com>
2023-01-04 12:32:13 +0100

    writing new txt files

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -4,5 +4,7 @@ const {readFileSync, writeFileSync} = fs
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
 
-console.log(first)
-console.log(second)
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result: \n\t1. ${first}\n\t2. ${second}`
+)

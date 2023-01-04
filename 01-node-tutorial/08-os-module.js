7783038217815f6fed7d91c678635c3ec02ac962
Raheem Kawojue <kawojue08@gmail.com>
2023-01-04 10:39:48 +0100

    [get] sys info

--- 01-node-tutorial/08-os-module.js
+++ 01-node-tutorial/08-os-module.js
@@ -4,3 +4,12 @@ const user = os.userInfo()
 console.log(user)
 
 console.log(`The System Uptime is ${os.uptime}sec.`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS)

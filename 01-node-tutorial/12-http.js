685ccd7fcebfff8ff4706342fd5ac01eef6bf030
Raheem Kawojue <kawojue08@gmail.com>
2023-01-05 12:20:53 +0100

    [write, check] req & res

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -1,8 +1,18 @@
 const http = require('http')
 
 const server  = http.createServer((req, res) => {
-    res.write("Welcome, user!")
-    res.end()
+    if (req.url === '/') {
+        res.end("Welcome, User!")
+    }
+
+    if (req.url === '/about') {
+        res.end("Welcome to our about page...")
+    }
+
+    res.end(`
+        <h1>Page not found!</h1>
+        <a href='/'>back home</a>
+    `)
 })
 
 server.listen(3030)

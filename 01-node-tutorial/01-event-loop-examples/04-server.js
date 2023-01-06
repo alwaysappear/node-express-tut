84eeda0978b6e2d6a2c28dab0f5e07acd7f14efa
Raheem Kawojue <kawojue08@gmail.com>
2023-01-06 17:13:16 +0100

    [use] http server

--- 01-node-tutorial/01-event-loop-examples/04-server.js
+++ 01-node-tutorial/01-event-loop-examples/04-server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   console.log('request event')
   if (req.url === "/") {
-    res.end('Hello World')
+    res.end('Hello, World!')
   }
 })
 

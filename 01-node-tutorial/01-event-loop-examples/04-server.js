03a003092bdb366ed34d1cde450cb3559776edd3
Raheem Kawojue <kawojue08@gmail.com>
2023-01-06 17:09:49 +0100

    [use] http server

---
+++ 01-node-tutorial/01-event-loop-examples/04-server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  console.log('request event')
+  if (req.url === "/") {
+    res.end('Hello World')
+  }
+})
+
+server.listen(3030, () => {
+  console.log('Server listening on port : 3030....')
+})

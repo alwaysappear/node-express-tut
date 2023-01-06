754576bfcb80d99609038dd695b4d1f23da28732
Raheem Kawojue <kawojue08@gmail.com>
2023-01-06 17:12:15 +0100

    [edit] file path

--- 01-node-tutorial/01-event-loop-examples/01-read-file.js
+++ 01-node-tutorial/01-event-loop-examples/01-read-file.js
@@ -2,7 +2,9 @@ const { readFile } = require('fs')
 
 console.log('started.')
 
-readFile('./content/first.txt', 'utf8', (err, result) => {
+readFile('../content/first.txt',
+'utf-8',
+(err, result) => {
   if (err) throw err
   console.log(result)
   console.log('completed.')

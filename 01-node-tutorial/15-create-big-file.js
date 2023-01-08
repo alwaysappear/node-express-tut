a48e226b001d8148245f0f3548d9e56ad6dd4b0a
Raheem Kawojue <kawojue08@gmail.com>
2023-01-08 20:10:43 +0100

    generate rand no. into TextFile

---
+++ 01-node-tutorial/15-create-big-file.js
@@ -0,0 +1,12 @@
+const {
+    writeFileSync
+} = require('fs')
+
+for (let i = 0; i <= 10000; i++) {
+    writeFileSync(
+        './content/big.txt',
+        `${Math.floor(Math.random() * 100)}\n`, {
+            flag: 'a'
+        }
+    )
+}

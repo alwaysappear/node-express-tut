17dec0ef855c435416975eaa8209612cdcb75a25
Raheem Kawojue <kawojue08@gmail.com>
2023-01-08 20:10:06 +0100

    .

--- 01-node-tutorial/app.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require('fs')
-
-for(let i = 0; i <= 10000; i++) {
-    writeFileSync(
-        './content/big.txt',
-        `${Math.floor(Math.random() * 100)}\n`,
-        {flag: 'a'}
-        )
-}

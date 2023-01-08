efbc5a2a6f9aaea7679661713eefbd901738fbab
Raheem Kawojue <kawojue08@gmail.com>
2023-01-08 20:25:05 +0100

    [load] stream file to [server] [using] events

--- 01-node-tutorial/17-http-streams.js
+++ 01-node-tutorial/17-http-streams.js
@@ -1,10 +1,15 @@
-const {readFileSync} = require('fs')
+const {
+    readFileSync,
+    createReadStream
+} = require('fs')
 const {createServer} = require('http')
 
 createServer((req, res) => {
-    const text = readFileSync(
-        './content/big.txt',
-        'utf-8'
-    )
-    res.end(text)
+    // const text = readFileSync(
+    //     './content/big.txt',
+    //     'utf-8'
+    // )
+    const fileStream = createReadStream('./content/big.txt', 'utf-8')
+    fileStream.on('open', () => fileStream.pipe(res))
+    fileStream.on('error', err => res.end(err))
 }).listen(3030)

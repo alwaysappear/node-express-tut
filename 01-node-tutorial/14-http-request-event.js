d40643821e7445ba9169c3c0619f0be8db9a69de
Raheem Kawojue <kawojue08@gmail.com>
2023-01-08 15:35:22 +0100

    created server events

--- 01-node-tutorial/14-http-request-event.js
+++ 01-node-tutorial/14-http-request-event.js
@@ -3,5 +3,9 @@ const http = require('http')
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-    console.log('Welcome!')
+    res.end('Welcome!')
+})
+
+server.listen(3030, () => {
+    console.log('Listening on port : 3030....')
 })

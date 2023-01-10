3fec46775e0a3d74691f9036cca5907e74380278
Raheem Kawojue <kawojue08@gmail.com>
2023-01-10 23:43:46 +0100

    conditional rendering

--- 02-express-tutorial/setup/01-http-basics.js
+++ 02-express-tutorial/setup/01-http-basics.js
@@ -1,8 +1,30 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write("<p>Hello, World!<p>")
+    // console.log(req.method)
+    
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write("<h1>Our Homepage.</h1>")
+    } else if (url === '/about') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.write("<h5>Our About Page.</h5>")
+    } else if (url === '/contact') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        })
+        res.write("Greetings - Hello, World!")
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        })
+        res.write('Page not found!')
+    }
     res.end()
 })
 .listen(1968, () => {

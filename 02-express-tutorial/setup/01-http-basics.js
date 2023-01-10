2a5c958a410cefd7d9a9041195e9866616e197f3
Raheem Kawojue <kawojue08@gmail.com>
2023-01-10 14:37:13 +0100

    [add] writeHead

--- 02-express-tutorial/setup/01-http-basics.js
+++ 02-express-tutorial/setup/01-http-basics.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-    res.end("Hello, World!")
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write("<p>Hello, World!<p>")
+    res.end()
 })
 .listen(1968, () => {
     console.log("Listening on port : 1968...")

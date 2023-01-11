e299fce7a8cc90c9910b96df2ef651216b744c9b
Raheem Kawojue <kawojue08@gmail.com>
2023-01-12 00:18:46 +0100

    express basics

--- 02-express-tutorial/setup/03-express-basics.js
+++ 02-express-tutorial/setup/03-express-basics.js
@@ -1,5 +1,17 @@
 const app = require('express')()
 
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome!')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page.')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send(`${req.url} does not exist.`)
+})
+
 
 app.listen(2003, () => {
     console.log('Server is running on port : 2003...')

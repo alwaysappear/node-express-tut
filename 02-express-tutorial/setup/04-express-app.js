bfc5172eea67a704f0ca54b6866a57b784d40a85
Raheem Kawojue <kawojue08@gmail.com>
2023-01-12 19:50:54 +0100

    [setup] static

--- 02-express-tutorial/setup/04-express-app.js
+++ 02-express-tutorial/setup/04-express-app.js
@@ -4,9 +4,7 @@ const app = express()
 
 app.use(express.static('../../public'))
 
-/* app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../navbar-app/index.html')))
-SSR
-*/
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../navbar-app/index.html')))
 
 app.all('*', (req, res) => res.status(404).send('Resource not found!'))
 

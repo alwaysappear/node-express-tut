80ccce0170d3ce30b2409c804eec1a6b3935e35c
Raheem Kawojue <kawojue08@gmail.com>
2023-01-12 19:51:08 +0100

    [setup] all statics

---
+++ 02-express-tutorial/setup/05-all-statics.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.static('../../public'))
+
+/* app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../navbar-app/index.html')))
+SSR
+*/
+
+app.all('*', (req, res) => res.status(404).send('Resource not found!'))
+
+app.listen(2003, () => console.log('Server is running on port : 2003...'))

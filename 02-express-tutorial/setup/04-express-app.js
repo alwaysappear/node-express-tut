f8e489eeda83477213ba403bedf3838054d5e073
Raheem Kawojue <kawojue08@gmail.com>
2023-01-12 01:03:47 +0100

    set 404

--- 02-express-tutorial/setup/04-express-app.js
+++ 02-express-tutorial/setup/04-express-app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 
+app.get('*', (req, res) => res.status(404).send('Resource not found!'))
+
 app.listen(2003, () => {
     console.log('Server is running on port : 2003...')
 })

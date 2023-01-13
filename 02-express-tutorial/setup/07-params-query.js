f732a9a7a58cd7e98a1ac0839ce08934cda529e7
Raheem Kawojue <kawojue08@gmail.com>
2023-01-13 01:51:39 +0100

    [get] single product

---
+++ 02-express-tutorial/setup/07-params-query.js
@@ -0,0 +1,27 @@
+const { products } = require('../data')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(
+`        <div>
+            <h3>Homepage.</h3>
+            <a href='/api/products'>Products</a>
+        </div>`
+    )
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map(product => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.status(200).json(newProducts)
+})
+
+app.get('/api/product/:id', ({ params: { id } }, res) => {
+    const singleProduct = products.find(product => product.id == id)
+    res.status(200).json(singleProduct)
+})
+
+app.listen(2003)

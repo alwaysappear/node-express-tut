eee546f8b939f045e014bcc9c3a1b710fe65791d
Raheem Kawojue <kawojue08@gmail.com>
2023-01-15 02:10:45 +0100

    [setup] query

--- 02-express-tutorial/setup/07-params-query.js
+++ 02-express-tutorial/setup/07-params-query.js
@@ -11,11 +11,6 @@ app.get('/', (req, res) => {
     )
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map(({id, name, image}) => ({id, name, image}))
-    res.status(200).json(newProducts)
-})
-
 app.get('/api/product/:id', ({ params: { id }}, res) => {
     const singleProduct = products.find(product => product.id === Number(id))
     
@@ -24,6 +19,32 @@ app.get('/api/product/:id', ({ params: { id }}, res) => {
     } else {
         res.status(404).json({message: 'Product not found!'})
     }
+})
+
+app.get('/api/products', ({
+    query: {
+        search,
+        limit
+    }
+}, res) => {
+    let newProducts = products.map(({ id, name, image }) => ({ id, name,image }))
+
+    if (limit) {
+        newProducts = newProducts.slice(0, Number(limit))
+    }
+
+    if (search) {
+        newProducts = newProducts.filter(product => {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if (newProducts.length < 1) {
+        res.status(200).json({success: true, data: []})
+    } else {
+        res.status(200).json(newProducts)
+    }
+
 })
 
 app.listen(2003)

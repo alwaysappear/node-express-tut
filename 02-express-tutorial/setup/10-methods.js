c71c47fd4ae5c25735571262f22e1969c14b2a8e
Raheem Kawojue <kawojue08@gmail.com>
2023-01-16 18:55:05 +0100

    [get] method

--- 02-express-tutorial/setup/10-methods.js
+++ 02-express-tutorial/setup/10-methods.js
@@ -1,4 +1,15 @@
 let { people } = require('../data')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+app.use(morgan('tiny'))
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people})
+})
+
+
+app.listen(2003, () => {
+    console.log('Listening on port : 2003...')
+})

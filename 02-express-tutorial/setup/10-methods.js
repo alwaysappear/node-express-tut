51bb6770fc94335ccefe37941df04a4e003ae5dd
Raheem Kawojue <kawojue08@gmail.com>
2023-01-18 02:05:19 +0100

    [add] urlencoded middleware

--- 02-express-tutorial/setup/10-methods.js
+++ 02-express-tutorial/setup/10-methods.js
@@ -4,13 +4,15 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.static('../methods-public'))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
 })
 
-
-
+app.post('/login', (req, res) => {
+    res.send('POST')
+})
 
 app.listen(2003, () => {
     console.log('Listening on port : 2003...')

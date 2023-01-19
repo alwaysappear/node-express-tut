d397d682cb56cf4a3442704b75e383d9541d6d9e
Raheem Kawojue <kawojue08@gmail.com>
2023-01-19 14:51:13 +0100

    .

--- 02-express-tutorial/setup/10-methods.js
+++ 02-express-tutorial/setup/10-methods.js
@@ -4,9 +4,9 @@ const app = express()
 const {static, urlencoded} = require('express')
 let { people } = require('../data')
 
+app.use(morgan('tiny'))
 app.use(static('../methods-public'))
 app.use(urlencoded({ extended: false }))
-app.use(morgan('tiny'))
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
@@ -20,6 +20,14 @@ app.post('/login', (req, res) => {
     }
 
     return res.status(401).send('Please, provide cridentials.')
+})
+
+app.post('api/people', (req, res) => {
+    const {name} = req.body
+    if (!name) {
+        return res.status(400).json({success: false, msg: 'Pls, provide name value'})
+    }
+    res.status(201).json({success: true, person: name})
 })
 
 app.listen(2003, () => {

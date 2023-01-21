3c956b2343458c4799903480ed05f47380eef5fb
Raheem Kawojue <kawojue08@gmail.com>
2023-01-21 01:14:29 +0100

    [put] method

--- 02-express-tutorial/setup/10-methods.js
+++ 02-express-tutorial/setup/10-methods.js
@@ -13,21 +13,29 @@ app.get('/api/people', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const {name} = req.body
+    const { name } = req.body
     
     if (name) {
         return res.status(200).send(`Welcome, ${name}!`)
     }
 
-    return res.status(401).send('Please, provide cridentials.')
+    res.status(401).send('Please, provide cridentials.')
 })
 
-app.post('api/people', (req, res) => {
-    const {name} = req.body
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
     if (!name) {
         return res.status(400).json({success: false, msg: 'Pls, provide name value'})
     }
     res.status(201).json({success: true, person: name})
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    console.log(id, name)
+    res.send('Hello, World!')
 })
 
 app.listen(2003)

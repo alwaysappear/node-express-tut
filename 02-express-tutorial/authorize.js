ca72e041ae9a5ec91443aa11568e7e048e34cfc7
Raheem Kawojue <kawojue08@gmail.com>
2023-01-29 01:37:18 +0100

    [edit] authorize

--- 02-express-tutorial/authorize.js
+++ 02-express-tutorial/authorize.js
@@ -1,7 +1,9 @@
+const { v4: uuid, v4 } = require('uuid')
+
 const authorize = (req, res, next) => {
     const { user } = req.query
-    if (user === 'raheem') {
-        req.user = {name: 'raheem', id: 1}
+    if (user) {
+        req.user = {name: user, id: v4()}
         next()
     } else {
         return res.status(401).json({message: 'Unauthorized'})

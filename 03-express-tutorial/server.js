c2e47b74788c41794cb7612499aeaebf206c07b0
Raheem Kawojue <kawojue08@gmail.com>
2023-01-29 14:34:17 +0100

    cors

--- 03-express-tutorial/server.js
+++ 03-express-tutorial/server.js
@@ -3,11 +3,28 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(cors()) // cross-origin resource share
+
 app.use(express.json())
 app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: false }))
 
+
+const whiteLists = [
+    'http://localhost:2003'
+]
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteLists.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS.'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions)) // cross-origin resource share
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`)

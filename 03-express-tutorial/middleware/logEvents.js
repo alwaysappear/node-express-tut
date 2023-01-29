f807a42796df9d6cb034942ce2ae2e6373a73a9b
Raheem Kawojue <kawojue08@gmail.com>
2023-01-29 16:02:23 +0100

    [add] log events

---
+++ 03-express-tutorial/middleware/logEvents.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const { appendFile } = require('fs').promises
+const { v4: uuid } = require('uuid')
+
+const textFile = path.resolve(__dirname, '../loggEvents.txt')
+
+const logger = (req, res, next) => {
+    appendFile(
+        textFile,
+        `id: ${uuid()}\n\tpath: \n\t\t${req.method} ${req.url}\n\n`,
+        (err) => {
+            if (err) throw new Error('Could not be resolved!')
+        }
+    )
+    next()
+}
+
+module.exports = logger

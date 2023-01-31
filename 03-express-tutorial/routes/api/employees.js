c23bfc3fb63a3e4ae9d73b1a5cb8510f7677e062
Raheem Kawojue <kawojue08@gmail.com>
2023-01-31 13:18:24 +0100

    REST API

--- 03-express-tutorial/routes/api/employees.js
+++ 03-express-tutorial/routes/api/employees.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { v4:uuid } = require('uuid')
 const path = require('path')
 
 const router = express.Router()
@@ -9,7 +10,26 @@ data.employees = require('../../data/employees.json')
 
 router.route('/')
     .get((req, res) => {
-        res.status(200).json({ success: true, data: data.employees })
+        res.json({
+            success: true,
+            employees: data.employees
+        })
+    })
+    .post((req, res) => {
+        res.json({
+            id: uuid(),
+            firsname: req.body.firsname,
+            lastname: req.body.lastname
+        })
+    })
+    .delete((req, res) => {
+        res.json({ "id": req.body.id })
+    });
+
+router.route('/:id')
+    .get(({ params: { id } }, res) => {
+        const newEmployee = data.employees.find(employee => employee.id === parseInt(id))
+        res.status(200).json({ newEmployee })
     })
 
 module.exports =  router

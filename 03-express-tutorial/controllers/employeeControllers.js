ded8bb9857a991347327577a9c91f1d6e2beea55
Raheem Kawojue <kawojue08@gmail.com>
2023-02-01 14:38:15 +0100

    [add func] check employee

--- 03-express-tutorial/controllers/employeeControllers.js
+++ 03-express-tutorial/controllers/employeeControllers.js
@@ -3,8 +3,24 @@ const { v4: uuid } = require('uuid')
 const data = {}
 data.employees = require('../data/employees.json')
 
+const checkEmployeeFunc = (id) => {
+    const employee = data.employees.find(emp => emp.id === parseInt(id))
+    if (!employee) {
+        return {
+            success: true,
+            message: `Employee with ID: ${id} could not be found!`
+        }
+    }
+    return employee
+}
+
 const getAllEmployees = (req, res) => {
-    res.status(200).json({ success: true, employess: data.employess })
+    res
+        .status(200)
+        .json({
+                success: true,
+                employess: data.employees
+        })
 }
 
 const getEmployee = ({
@@ -12,17 +28,23 @@ const getEmployee = ({
         id
     }
 }, res) => {
-    const newEmployee = data.employees.find(employee => employee.id === parseInt(id))
-    res.status(200).json({
-        newEmployee
-    })
+    if (checkEmployeeFunc(id)) {
+        return res
+            .status(400)
+            .json(checkEmployeeFunc(id))
+    }
+    res
+        .status(200)
+        .json(checkEmployeeFunc(id))
 }
 
-const addNewEmployee = (res, res) => {
-    res.json({
-        id: uuid(),
-        firsname: req.body.firsname,
-        lastname: req.body.lastname
+const addNewEmployee = (req, res) => {
+    res
+        .status(201)
+        .json({
+            id: uuid(),
+            firsname: req.body.firsname,
+            lastname: req.body.lastname
     })
 }
 
@@ -31,11 +53,22 @@ const deleteEmployee = ({
         id
     }
 }, res) => {
+    if (checkEmployeeFunc(id)) {
+        return res
+                .status(400)
+                .json(checkEmployeeFunc(id))
+    }
     const newEmployees = data.employees.filter(employee => employee.id !== parseInt(id))
-    res.status(200).json({
-        success: true,
-        employees: newEmployees
+    res
+        .status(200)
+        .json({
+            success: true,
+            employees: newEmployees
     })
 }
 
-module.exports = { getAllEmployees, getEmployee, deleteEmployee, addNewEmployee }
+module.exports = {
+    getAllEmployees, getEmployee,
+    deleteEmployee, addNewEmployee,
+    updateEmployee
+}

da7224e6de5f8d30d5df0f4e9fca434dffe53170
Raheem Kawojue <kawojue08@gmail.com>
2023-02-01 15:22:42 +0100

    [add] update data

--- 03-express-tutorial/controllers/employeeControllers.js
+++ 03-express-tutorial/controllers/employeeControllers.js
@@ -53,18 +53,30 @@ const deleteEmployee = ({
         id
     }
 }, res) => {
-    if (checkEmployeeFunc(id)) {
-        return res
-                .status(400)
-                .json(checkEmployeeFunc(id))
-    }
     const newEmployees = data.employees.filter(employee => employee.id !== parseInt(id))
-    res
+    return res
         .status(200)
         .json({
             success: true,
             employees: newEmployees
-    })
+        })
+    
+}
+
+const updateEmployee = (req, res) => {
+    if (checkEmployeeFunc(id)) {
+        return res
+            .status(400)
+            .json(checkEmployeeFunc(id))
+    }
+    const checkEmployee = checkEmployeeFunc(id)
+
+    if (req.body.firsname) employee.firsname = req.body.firsname
+    if (req.body.lastname) employee.lastname = req.body.lastname
+    const filteredArray = data.employees.filter(employee => employee.id !== parseInt(req.body.id))
+    const newEmployees = [...filteredArray, checkEmployee]
+    data.setEmployees(filteredArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+    res.json(data.employees)
 }
 
 module.exports = {

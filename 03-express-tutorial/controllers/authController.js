133a7dcf5ef38af484a20f45c03761a48330ca1f
Raheem Kawojue <kawojue08@gmail.com>
2023-02-05 12:36:50 +0100

    refractored

--- 03-express-tutorial/controllers/authController.js
+++ 03-express-tutorial/controllers/authController.js
@@ -1,11 +1,5 @@
 const bcrypt = require('bcrypt')
-
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) {
-        this.users = data
-    }
-}
+const { usersDB } = require('../model/usersDB')
 
 const handleLogin = async (req, res) => {
     const { user, pswd } = req.body

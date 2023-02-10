90d85731ec7970100dffc783c7ab1360f67006df
Raheem Kawojue <kawojue08@gmail.com>
2023-02-10 13:57:33 +0100

    clear cookie

--- 03-express-tutorial/controllers/logoutController.js
+++ 03-express-tutorial/controllers/logoutController.js
@@ -2,19 +2,19 @@ const { usersDB } = require('../model/usersDB')
 const { writeFile } = require('fs').promises
 const { resolve } = require('path')
 
+const clearCookie = {
+    httpOnly: true,
+    sameSite: 'None',
+    secure: true,
+}
+
 const handleLogout = async (req, res) => {
     const cookies = req.cookies
     if (!cookies?.jwt) return res.sendStatus(204) // No content
     const refreshToken = cookies.jwt
     const userExists = usersDB.users.find(user => user.refreshToken === refreshToken)
     if (!userExists) {
-        res.clearCookie(
-            'jwt',
-            {
-                httpOnly: true,
-                maxAge: 7 * 24 * 60 * 60 * 1000
-            }
-        )
+        res.clearCookie('jwt', clearCookie)
         res.sendStatus(204)
     }
     const updateUser =  usersDB.users.map(user => user.refreshToken === userExists.refreshToken ? {...user, refreshToken: ""}: user)
@@ -23,12 +23,7 @@ const handleLogout = async (req, res) => {
         resolve(__dirname, '../model/users.json'),
         JSON.stringify(usersDB.users)
     )
-    res.clearCookie(
-        'jwt', {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        }
-    )
+    res.clearCookie('jwt', clearCookie)
     res.sendStatus(204)
 }
 

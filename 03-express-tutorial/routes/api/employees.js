190bbfa928e3884da8d37845173ae93d28435954
Raheem Kawojue <kawojue08@gmail.com>
2023-02-11 15:29:22 +0100

    [add] authorization

--- 03-express-tutorial/routes/api/employees.js
+++ 03-express-tutorial/routes/api/employees.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const verifyJWT = require('../../middleware/verifyJWT')
+const ROLES_LIST = require('../../config/roles_list')
+const verifyRoles = require('../../middleware/verifyRoles')
 
 const {
     getAllEmployees, getEmployee,
@@ -12,11 +14,11 @@ router.use(verifyJWT)
 
 router.route('/')
     .get(getAllEmployees)
-    .post(addNewEmployee)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), addNewEmployee)
     .put(updateEmployee)
 
 router.route('/:id')
     .get(getEmployee)
-    .delete(deleteEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee)
 
 module.exports =  router

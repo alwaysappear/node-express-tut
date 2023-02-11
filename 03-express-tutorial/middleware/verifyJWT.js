ea0a417fb25749352e6efa70eb27cdf76aa836a3
Raheem Kawojue <kawojue08@gmail.com>
2023-02-11 14:00:12 +0100

    [decoded] roles

--- 03-express-tutorial/middleware/verifyJWT.js
+++ 03-express-tutorial/middleware/verifyJWT.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization']
-    if (!authHeader) return res.sendStatus(401) // unauthorized
+    const authHeader = req.headers[`${'a' || 'A'}uthorization`]
+    if (!authHeader?.startsWith('Bearer')) return res.sendStatus(401) // unauthorized
     const token = authHeader.split(' ')[1]
     jwt.verify(
         token,
@@ -11,6 +11,7 @@ const verifyJWT = (req, res, next) => {
         (err, decoded) => {
             if (err) return res.sendStatus(403) // forbidden
             req.user = decoded.username
+            req.roles = decoded.userinfo.roles
             next()
         }
     )
